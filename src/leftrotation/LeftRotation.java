1fdbc706230e05398531935b5b23806d876abbb4
Luis Felipe <luisfelipekai@gmail.com>
2017-09-18 20:55:31 -0300

    Left Rotation

---
+++ src/leftrotation/LeftRotation.java
@@ -0,0 +1,35 @@
+package leftrotation;
+import java.util.*;
+
+public class LeftRotation {
+
+	static int[] leftRotation(int[] a, int d) {
+        // Complete this function
+        int[] leftRotatedArray = new int[a.length];
+        for (int i=0; i<a.length;i++){
+            int diff = i-d;
+            int leftIndex = diff >= 0 ? diff : (a.length-java.lang.Math.abs(diff))% a.length;
+            leftRotatedArray[leftIndex]=a[i];
+        }
+        return leftRotatedArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int d = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i = 0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int[] result = leftRotation(a, d);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+
+}
