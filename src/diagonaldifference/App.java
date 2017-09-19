2ae902703b1d8abdb27a7027a88f14df27a434bd
Luis Felipe <luisfelipekai@gmail.com>
2017-09-19 05:12:13 -0300

    Remove comments

--- src/diagonaldifference/App.java
+++ src/diagonaldifference/App.java
@@ -14,16 +14,12 @@ public class App {
         ArrayList<Integer>[] arr = new ArrayList[n];
         int diagonal1=0;
         int diagonal2=0;
-        int count1=0;
-        int count2=n-1;
         for (int i=0; i<n; i++){
             arr[i]=new ArrayList<Integer>();
             ArrayList<String> inputValues = new ArrayList<String>(Arrays.asList(sc.nextLine().split(" ")));
             arr[i]= (ArrayList<Integer>) inputValues.stream().map(s-> Integer.parseInt(s)).collect(Collectors.toList());
             diagonal1=diagonal1+arr[i].get(i);
             diagonal2=diagonal2+arr[i].get(n-i-1);
-            //count1++;
-            //count2--;
         }
         int difference=diagonal1-diagonal2;
         System.out.println(Math.abs(difference));
